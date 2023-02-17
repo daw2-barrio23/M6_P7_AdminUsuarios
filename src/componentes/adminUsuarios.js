343357afe22e5d0afbe2cb2f9907f230d8ec4b25
barriomunoznicolas@fpllefia.com <barriomunoznicolas@fpllefia.com>
2023-02-17 15:34:57 +0100

    ejercicio 3 hecho

---
+++ src/componentes/adminUsuarios.js
@@ -0,0 +1,20 @@
+export const arrayUsers = [
+    {
+      id: 1,
+      nick: "Nico",
+      email: "nico@gmail.com",
+      pass: "nico1"
+    },
+    {
+      id: 2,
+      nick: "Pau",
+      email: "pau@gmail.com",
+      pass: "pau2"
+    },
+    {
+      id: 3,
+      nick: "Jose",
+      email: "jose@gmail.com",
+      pass: "jose3"
+    }
+  ];
--- src/vistas/admin.js
+++ src/vistas/admin.js
@@ -1,6 +1,62 @@
+import {arrayUsers} from "../componentes/adminUsuarios"
+
 export const admin ={
-    template: `<h2>Pagina Admin</h2> `,
-    script: ()=>{
-        console.log('Pagina Admin')
-    }
+    template:`
+    <table class="table" id="tabla">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Nick</th>
+          <th>Email</th>
+          <th>Borrar</th>
+          <th>Editar</th>
+        </tr>
+      </thead>
+      <tbody id="cuerpo">
+  
+      </tbody>
+    </table>
+    `,
+    script:()=>{
+  
+        let html=``
+  
+        arrayUsers.forEach(arrayUsers => {
+          html+=`    
+          <tr id="${arrayUsers.id}">
+            <th >${arrayUsers.id}</th>
+            <td>${arrayUsers.nick}</td>
+            <td>${arrayUsers.email}</td>
+            <td><button type="button" class="btn btn-danger eliminar" >Eliminar</button></td>
+            <td><button type="button" class="btn btn-info editar">Editar</button></td>
+          </tr>
+          `
+        });
+    
+        document.querySelector('#cuerpo').innerHTML = html
+
+
+
+
+
+        const botonesEliminar = document.querySelectorAll(".eliminar");
+
+        botonesEliminar.forEach(botonEliminar => {
+          botonEliminar.addEventListener("click", () => {
+            alert(`Se hizo clic en el botón eliminar`);
+
+            const fila = botonEliminar.parentNode.parentNode;
+            fila.classList.add("fila-oculta");
+          });
+        });
+
+
+        const botonesEditar = document.querySelectorAll(".editar");
+
+        botonesEditar.forEach(botonEditar => {
+          botonEditar.addEventListener("click", () => {
+            alert(`Se hizo clic en el botón editar`);
+          });
+        });
+      }
 }
